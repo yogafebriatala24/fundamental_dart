0361b3cf81d64fdea3ed5018ba3d3e3c11c53e7f
Yoga Febriatala <yogafebriatala24@gmail.com>
2022-05-22 01:08:47 +0700

    clear

---
+++ bin/function/recursive.dart
@@ -0,0 +1,32 @@
+int factorialLoop(int value) {
+  var angka = 1;
+  for (var i = 1; i <= value; i++) {
+    angka *= i;
+  }
+
+  return angka;
+}
+
+int factorialRecursive(int value) {
+  if (value == 1) {
+    return 1;
+  } else {
+    return value * factorialRecursive(value - 1);
+  }
+}
+
+void loop(int value) {
+  if (value == 0) {
+    print('Selesai');
+  } else {
+    print('Perulangan ke-$value');
+    loop(value - 1);
+  }
+}
+
+void main() {
+  print(factorialLoop(10));
+  print(factorialRecursive(10));
+  //factorialRecursive(10) //fac(10) => 10 * fac(9) => 10 * 9 * fac(8) dst..
+  loop(10);
+}
